008ba735d9a72600bbfe221a44cb2df23528cb7c
Yelloooowww <cococing0324@gmail.com>
2021-10-15 14:02:19 +0800

    specify datasize

--- hw1/plot.py
+++ hw1/plot.py
@@ -10,8 +10,6 @@ path_list = ['multithread_int.csv', 'multiprocess_int.csv', \
 				'multithread_struct.csv', 'multiprocess_struct.csv']
 
 for i,path in enumerate(path_list):
-	plt.clf()
-	
 	X,Y,Z = [],[],[] #X:datasize, Y:thread, Z:time
 	with open(path, newline='') as csvfile:
 		rows = csv.reader(csvfile, delimiter=',')
@@ -22,9 +20,16 @@ for i,path in enumerate(path_list):
 			Z.append(int(row[2]))
 
 	plot_x, plot_y = [[] for i in range(12)], [[] for i in range(12)]
+	spec_datasize_plot_x = [[] for i in range(int(math.log(100000000,2)))]
+	spec_datasize_plot_y = [[] for i in range(int(math.log(100000000,2)))]
+
+	### specify thread/process ###
 	for total_points in range(len(X)):
 		plot_x[Y[total_points]-1].append(X[total_points])
 		plot_y[Y[total_points]-1].append(Z[total_points])
+
+		spec_datasize_plot_x[int(math.log(X[total_points],2))-1].append(Y[total_points])
+		spec_datasize_plot_y[int(math.log(X[total_points],2))-1].append(Z[total_points])
 
 	for thread in range(12): plt.plot(plot_x[thread],plot_y[thread], '.-')
 
@@ -37,6 +42,29 @@ for i,path in enumerate(path_list):
 		plt.legend([str(i+1)+" process" for i in range(12)])
 	plt.xlabel("datasize")
 	plt.ylabel("time")
-	plt.title(path)
+	plt.title(path[:-4])
 	plt.savefig(path[:-4]+".png")
 	# plt.show()
+	plt.clf()
+
+
+	### specify thread/process ###
+	legend_list = []
+	for k in range(int(math.log(100000000,2))):
+		if (k>=15) and (k<25):
+			legend_list.append("datasize=2^"+str(k))
+			plt.plot(spec_datasize_plot_x[k],spec_datasize_plot_y[k], '.-')
+
+	# plt.yscale("log", basex=2)
+	plt.grid()
+	plt.legend(legend_list)
+	# plt.legend(["datasize=2^"+str(k+1) for k in range(int(math.log(100000000,2)),3)])
+	if i%2==0:
+		plt.xlabel("thread")
+	else:
+		plt.xlabel("process")
+
+	plt.ylabel("time")
+	plt.title("specify_datasize "+path[:-4])
+	plt.savefig("specify_datasize "+path[:-4]+".png")
+	plt.clf()
